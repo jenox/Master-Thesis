4844ff9ee1e982f22ba3ddfec7a5ea8bbb2351d6
Christian Schnorr <christian.schnorr@me.com>
2020-01-12 15:43:40 +0100

    Initial commit

---
+++ MasterThesis/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  MasterThesis
+//
+//  Created by Christian Schnorr on 12.01.20.
+//  Copyright © 2020 Christian Schnorr. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+
+        self.window = window
+
+        window.makeKeyAndVisible()
+
+        return true
+    }
+}
---
+++ MasterThesis/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  MasterThesis
+//
+//  Created by Christian Schnorr on 12.01.20.
+//  Copyright © 2020 Christian Schnorr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        self.view.backgroundColor = .white
+    }
+}
