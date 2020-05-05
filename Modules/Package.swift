221d47c581cfe0ad1bd17b9c61f91a4b51bd407e
Christian Schnorr <christian.schnorr@me.com>
2020-05-05 08:55:59 +0200

    Remove experimental stuff

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -7,15 +7,12 @@ let package = Package(
     name: "Modules",
     platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
-        .executable(name: "Executable", targets: ["Executable"]),
         .library(name: "Geometry", targets: ["Geometry"]),
         .library(name: "Collections", targets: ["Collections"]),
     ],
     dependencies: [
     ],
     targets: [
-        .target(name: "Executable", dependencies: ["Geometry", "Collections"]),
-
         .target(name: "Geometry", dependencies: ["Collections"]),
         .target(name: "Collections", dependencies: []),
 
