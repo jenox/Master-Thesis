a4746c224884ef652a0c88e2e71d1735b1bc8513
Christian Schnorr <christian.schnorr@me.com>
2020-05-26 21:21:13 +0200

    Migrate evaluation script to SPM

--- Implementation/Modules/Package.swift
+++ Implementation/Modules/Package.swift
@@ -11,14 +11,19 @@ let package = Package(
         .library(name: "Collections", targets: ["Collections"]),
         .library(name: "Delaunay", targets: ["Delaunay"]),
         .library(name: "Framework", targets: ["Framework"]),
+
+        .executable(name: "Evaluation", targets: ["Evaluation"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.0.6"),
     ],
     targets: [
         .target(name: "Geometry", dependencies: []),
         .target(name: "Collections", dependencies: []),
         .target(name: "Delaunay", dependencies: []),
         .target(name: "Framework", dependencies: ["Geometry", "Collections", "Delaunay"]),
+
+        .target(name: "Evaluation", dependencies: ["Framework", "Collections", .product(name: "ArgumentParser", package: "swift-argument-parser")]),
 
         .testTarget(name: "CollectionsTests", dependencies: ["Collections"]),
     ]
