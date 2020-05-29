7a0945b8b0bf7b7733181661bd170a51053ae712
Christian Schnorr <christian.schnorr@me.com>
2020-05-29 02:10:49 +0200

    Experimental evaluation!

--- Scripts/plot.py
+++ Scripts/plot.py
@@ -20,11 +20,9 @@ inputfilenames = [
 ]
 frames = map(lambda x: pandas.read_csv("{0}/{1}.csv".format(inputdir, x)), inputfilenames)
 data = pandas.concat(frames)
-data["nesting ratio and bias"] = "(" + data["nesting ratio"].astype(str) + ", " + data["nesting bias"].astype(str) + ")"
 
-sizes = [10, 15, 20, 25, 30]
-nestings = [(0.0, 0.0), (0.25, 0.0), (0.25, 0.5), (0.25, 0.99), (0.5, 0.0), (0.5, 0.5), (0.5, 0.99)]
-times = [0, 2, 4, 6, 10, 20]
+data["initial number of clusters"] = data["number of vertices"]
+data["nesting ratio and bias"] = "(" + data["nesting ratio"].transform(str) + ", " + data["nesting bias"].transform(str) + ")"
 
 metrics = [
     ("maximum cartographic error", "MaximumCartographicError"),
@@ -33,31 +31,40 @@ metrics = [
     ("average polygon complexity", "AveragePolygonComplexity"),
 ]
 
-seaborn.set(rc = { 'figure.figsize': (8, 6) })
+seaborn.set(rc = { "figure.figsize": (7, 5.25) })
 
 print("Creating plots for variable number of operations…")
-for n in sizes:
-    for (a, b) in nestings:
+for n in [20]:
+    for (a, b) in [(0,0)]:
         suffix = "t=?,n={0},a={1},b={2}".format(n, a, b)
-        filtered = data[data["number of vertices"].eq(n) & data["nesting ratio"].eq(a) & data["nesting bias"].eq(b) & data["number of operations"].isin(times)]
         for (column, prefix) in metrics:
-            pyplot.clf()
-            seaborn.swarmplot(y=column, x="number of operations", data=filtered).get_figure().savefig("{0}/{1}-{2}.pdf".format(outputdir, prefix, suffix))
+            if column.find("error") != -1:
+                filtered = data[data["initial number of clusters"].eq(n) & data["nesting ratio"].eq(a) & data["nesting bias"].eq(b) & data["number of operations"].isin([0,1,2,5,10,20])]
+                pyplot.clf()
+                chart = seaborn.swarmplot(y=column, x="number of operations", data=filtered, size=4.375)
+                chart.get_figure().savefig("{0}/{1}-{2}.pdf".format(outputdir, prefix, suffix), bbox_inches="tight", pad_inches=0)
+            else:
+                filtered = data[data["initial number of clusters"].eq(n) & data["nesting ratio"].eq(a) & data["nesting bias"].eq(b) & data["number of operations"].isin([0,4,8,12,16,20])]
+                pyplot.clf()
+                chart = seaborn.swarmplot(y=column, x="number of operations", data=filtered, size=4.375)
+                chart.get_figure().savefig("{0}/{1}-{2}.pdf".format(outputdir, prefix, suffix), bbox_inches="tight", pad_inches=0)
 
 print("Creating plots for variable nesting ratio and bias…")
-for n in sizes:
-    for t in times:
+for n in [20]:
+    for t in [0]:
         suffix = "a=?,b=?,n={0},t={1}".format(n, t)
-        filtered = data[data["number of vertices"].eq(n) & data["number of operations"].eq(t)]
+        filtered = data[data["initial number of clusters"].eq(n) & data["number of operations"].eq(t)]
         for (column, prefix) in metrics:
             pyplot.clf()
-            seaborn.swarmplot(y=column, x="nesting ratio and bias", data=filtered).get_figure().savefig("{0}/{1}-{2}.pdf".format(outputdir, prefix, suffix))
+            chart = seaborn.swarmplot(y=column, x="nesting ratio and bias", data=filtered, size=4.375)
+            chart.get_figure().savefig("{0}/{1}-{2}.pdf".format(outputdir, prefix, suffix), bbox_inches="tight", pad_inches=0)
 
 print("Creating plots for variable number of vertices…")
-for (a, b) in nestings:
-    for t in times:
-        suffix = "n=?,a={0},b={0},t={2}".format(a, b, t)
+for (a, b) in [(0,0)]:
+    for t in [0]:
+        suffix = "n=?,a={0},b={1},t={2}".format(a, b, t)
         filtered = data[data["nesting ratio"].eq(a) & data["nesting bias"].eq(b) & data["number of operations"].eq(t)]
         for (column, prefix) in metrics:
             pyplot.clf()
-            seaborn.swarmplot(y=column, x="number of vertices", data=filtered).get_figure().savefig("{0}/{1}-{2}.pdf".format(outputdir, prefix, suffix))
+            chart = seaborn.swarmplot(y=column, x="initial number of clusters", data=filtered, size=4.375)
+            chart.get_figure().savefig("{0}/{1}-{2}.pdf".format(outputdir, prefix, suffix), bbox_inches="tight", pad_inches=0)
